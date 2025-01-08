b41a75d97939d444c405e23f6f11280ad5dee731
murahiro <murahiro0919@gmail.com>
2025-01-08 10:51:09 +0900

    Projectモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_07_221813) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_08_014256) do
+  create_table "projects", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "place", null: false
+    t.datetime "start_at"
+    t.datetime "end_at"
+    t.datetime "deleted_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
---
+++ spec/factories/projects.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :project do
+    name { "MyString" }
+    place { "MyString" }
+    start_at { "2025-01-08 10:42:56" }
+    end_at { "2025-01-08 10:42:56" }
+    deleted_at { "2025-01-08 10:42:56" }
+  end
+end
