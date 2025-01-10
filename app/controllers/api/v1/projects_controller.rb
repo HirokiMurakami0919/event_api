e44b0d9d55f349e3c4d9ee3e9cd471798766e8dd
murahiro <murahiro0919@gmail.com>
2025-01-10 13:53:41 +0900

    improve: projects_controller.rbを修正

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,35 +1,44 @@
-class Api::V1::ProjectsController < ApplicationController
-  def show
-    project = Project.find(id: params[:id])
+# app/controllers/api/v1/projects_controller.rb
+module Api
+  module V1
+    class ProjectsController < ApplicationController
+      def show
+        project = Project.find_by(id: params[:id])
+        if project
+          render json: project_response(project), status: :ok
+        else
+          render json: { error: 'Project not found' }, status: :not_found
+        end
+      end
 
-    if project
-      render json: project_response(project), status: :ok
-    else
-      render json: { error: 'Project not found'}, status: :not_found
-    end
-  end
+      private
 
-  private
-
-  def project_response(project)
-    {
-      id: project.id,
-      name: project.name,
-      place: project.place,
-      start_at: project.start_at,
-      end_at: project.end_at,
-      members: project.users.map do |user|
+      def project_response(project)
         {
-          id: user.id,
-          name: user.name,
-          email: user.email,
-          tel: user.tel,
-          role: user.role,
-          last_sign_in_at: user.last_sign_in_at,
-          created_at: user.created_at,
-          updated_at: user.updated_at
+          id: project.id,
+          name: project.name,
+          place: project.place,
+          start_at: project.start_at,
+          end_at: project.end_at,
+          created_at: project.created_at,
+          updated_at: project.updated_at,
+          deleted_at: project.deleted_at,
+          members: project.members.includes(:user).map do |member|
+            user = member.user
+            {
+              id: user.id,
+              name: user.name,
+              email: user.email,
+              tel: user.tel,
+              role: user.role,
+              last_sign_in_at: user.last_sign_in_at,
+              created_at: user.created_at,
+              updated_at: user.updated_at,
+              deleted_at: nil
+            }
+          end
         }
       end
-    }
+    end
   end
 end
