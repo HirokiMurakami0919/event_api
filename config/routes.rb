792610ab3821e2c7c8a69d6fb190b1a1ea37219b
murahiro <murahiro0919@gmail.com>
2025-01-10 17:23:49 +0900

    add: ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :projects, only: [:show]
+      resources :projects, only: [:show, :create]
       post 'auth', to: 'auth#create'
     end
   end
